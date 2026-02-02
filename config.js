517d4b287e8355217b8b73648ebada5ab56c45c7
Yan Russel Tatchum Nono <44243880+TatchumNono@users.noreply.github.com>
2026-02-02 21:19:15 +0100

    Update music streaming URL in config.js

--- config.js
+++ config.js
@@ -79,7 +79,8 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        // musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dmvbw3bqr/video/upload/v1770063344/dave_raindance_ft._tems_mp3_72860_u9nmdd.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
